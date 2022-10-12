83bd9ad1dde1e526bcda2d06484c9338c5564077
Juan Duque <juuanduuke@gmail.com>
2022-10-11 22:17:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,73 @@
-import  express from 'express';
-import dotenv from 'dotenv';
-import { USERS_BBDD } from './bd/bbdd.js';
+import  express from 'express'
+import dotenv from 'dotenv'
+import { USERS_BBDD } from './bd/bbdd.js'
 
-dotenv.config();
+dotenv.config()
 
-const PORT = process.env.PORT;
-
-const expressApp = express();
+const expressApp = express()
 
 expressApp.use(express.json())
 expressApp.use(express.text())
 
 //Obtener los detalles de una cuenta a partir del guid
-expressApp.get()
+expressApp.get('/account/:guid', (req, res) => {
+    const { guid } = req.params;
+
+    const user = USERS_BBDD.find( user => user.guid === guid)
+    
+    if (!user) return res.status(404).send()
+
+    return res.send(user)
+})
 
 //Crear una nueva cuenta
-expressApp.post()
+expressApp.post('/account', (req, res) => {
 
-//Actualizar una cuenta
-expressApp.put()
+    const { name, guid } = req.body
+
+    if (!name || !guid) return res.status(400).send();
+
+    const user = USERS_BBDD.find( user => user.guid === guid)
+    
+    if (user) return res.status(409).send()
+
+    USERS_BBDD.push({
+        name, guid
+    })
+
+    return res.send()
+})
+
+//Actualizar el nombre de una cuenta
+expressApp.put('/account/:guid', (req, res) => {
+    const { guid } = req.params
+
+    const { name } = req.body
+
+    if (!name) return res.status(400).send();
+
+    const user = USERS_BBDD.find( user => user.guid === guid)
+    
+    if (!user) return res.status(404).send()
+
+    user.name = name
+
+    return res.send(user)
+})
 
 //Eliminar una cuenta
-expressApp.delete()
+expressApp.delete('/account/:guid', (req, res) => {
+    const { guid } = req.params;
 
-expressApp.listen(PORT, () => {
-    console.log(`Servidor levantado en el ${PORT}`)
+    const userIndex = USERS_BBDD.findIndex( user => user.guid === guid)
+    
+    if (userIndex === -1) return res.status(404).send()
+
+    USERS_BBDD.splice(userIndex, 1)
+
+    return res.send()
+})
+
+expressApp.listen(process.env.PORT, () => {
+    console.log(`Servidor levantado en el ${process.env.PORT}`)
 })
