399395f4add0a4d8e1babf09624c78bd98d24b03
Juan Duque <juuanduuke@gmail.com>
2022-11-01 21:03:15 -0600

    Agregando mongoose para uso de BD NoSql

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import  express from 'express'
 import dotenv from 'dotenv'
 import accountRouter from './src/routes/account.js'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
@@ -15,6 +16,12 @@ expressApp.get('/', (req, res) => {
 
 expressApp.use("/account", accountRouter)
 
-expressApp.listen(process.env.PORT, () => {
-    console.log(`Servidor levantado en el ${process.env.PORT}`)
-})
+const bootstrp = async () => {
+    await mongoose.connect(process.env.MONGODB_URL)
+
+    expressApp.listen(process.env.PORT, () => {
+        console.log(`Servidor levantado en el ${process.env.PORT}`)
+    })
+}
+
+bootstrp()
